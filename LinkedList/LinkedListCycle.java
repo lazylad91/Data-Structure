d524e5d83cbdc76164bceb8fd8b0990ef41c11be
Parteek Makkar <parteek.makkar@sjsu.edu>
2016-10-10 23:22:37 +0000

    Detecting cycle in linkedlist

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,25 @@
+/*Given a linked list, determine if it has a cycle in it.*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        ListNode walker = head;
+        ListNode runner = head;
+        while(runner.next!=null && runner.next.next!=null){
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker==runner) return true;
+        }
+        return false;
+    }
+}

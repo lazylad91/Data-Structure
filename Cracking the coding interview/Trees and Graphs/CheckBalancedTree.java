dbd014f4cd74e9840111053b06526f5926980ea2
Parteek Makkar <parteek.makkar@sjsu.edu>
2017-03-23 22:44:56 +0000

    Find whether the tree is symmetric or not

--- Cracking the coding interview/Trees and Graphs/CheckBalancedTree.java
+++ Cracking the coding interview/Trees and Graphs/CheckBalancedTree.java
@@ -1,4 +1,11 @@
 /**
+ * Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Subscribe to see which companies asked this question.
+
+
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -7,6 +14,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
  
  /*
  So first solution is write a function to max depth of tree

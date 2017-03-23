20090a58a983d6f8d57deb754b4fd0d7fda8d091
Parteek Makkar <parteek.makkar@sjsu.edu>
2017-03-23 22:20:11 +0000

    Find whether the tree is balanced or not

---
+++ Cracking the coding interview/Trees and Graphs/CheckBalancedTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+ /*
+ So first solution is write a function to max depth of tree
+ then find depth of left sub tree n right if diff(>1 ) then return false
+ else isBalanced(leftree) && isBalanced(rightre
+ e)
+ It will take n square 
+ so lets do it the right way which is we will start from the end
+ and find whether the last subtree is balanced and make our way up
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+       return dfsHeight(root) != -1;
+    }
+    
+    public int dfsHeight(TreeNode root){
+        
+        if(root==null)
+        return 0;
+        
+        int leftHeight = dfsHeight(root.left);
+        if(leftHeight==-1) return -1;
+        
+        int rightHeight = dfsHeight(root.right);
+        if(rightHeight==-1) return -1;
+        
+        if(Math.abs(leftHeight-rightHeight)>1) return -1;
+        else return Math.max(leftHeight,rightHeight)+1;
+    }
+}

916dcfa59086b7740307e50fb1563544a646d0c0
Parteek Makkar <parteek.makkar@sjsu.edu>
2017-03-24 19:07:48 +0000

    Find the bottom most left leave

--- Cracking the coding interview/Trees and Graphs/FindDiameterBinaryTree.java
+++ Cracking the coding interview/Trees and Graphs/FindDiameterBinaryTree.java
@@ -1,6 +1,6 @@
 /*Diameter of a Binary Tree
 The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between two leaves in the tree. The diagram below shows two trees each with diameter nine, the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
-
+but in this solution we are taking number of edges
 
 
 The diameter of a tree T is the largest of the following quantities:
@@ -26,9 +26,9 @@ public class FindDiameterBinaryTree {
         
         int leftdepth = findMaxDepth(root.left);
         int rightdepth = findMaxDepth(root.right);
-        
+        // this is we are taking number of edges thats leftdepth+rightdepth
         max = Math.max(max, leftdepth+rightdepth);
-        
+        // this is actual depth that is number of nodes
         return Math.max(leftdepth,rightdepth)+1;
     }
 }
